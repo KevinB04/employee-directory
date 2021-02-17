544798b3e0650dc423c4464056f804d3d6de6759
Kevin Buttimer <kbuttimer4@gmail.com>
2021-02-17 14:47:35 -0500

    correcting code in search.jsx

--- employee-directory/src/components/Search/search.jsx
+++ employee-directory/src/components/Search/search.jsx
@@ -1,24 +1,11 @@
 import React from "react";
 
-function Search(props) {
+const Search = () => {
   return (
-    <form className="search">
-      <div className="form-group">
-        <input
-          value={props.search}
-          onChange={props.handleInputChange}
-          name="search"
-          type="text"
-          className="form-control"
-          placeholder="Search for an Employee"
-          id="search"
-        />    
-        <button type="submit" onClick={props.handleFormSubmit} className="btn btn-success">
-          Search
-        </button>
-      </div>
-    </form>
+    <div>
+      <h1>Search</h1>
+    </div>
   );
-}
+};
 
 export default Search;
