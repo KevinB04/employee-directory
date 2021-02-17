96fca1dfeb963fec65f0dc3752d0a5af1b1969bb
Kevin Buttimer <kbuttimer4@gmail.com>
2021-02-16 19:59:42 -0500

    finishing code for header.jsx

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -3,16 +3,7 @@ import Header from "./components/Header/header";
 
 function App() {
   return (
-    <div>
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <h1 className="text-center">Employee Directory</h1>
-            <p>Click on carrots to filter by heading or use the search box to narrow your results.</p>
-          </div>
-        </div>
-      </div>
-    </div>
+      
   );
 }
 
--- employee-directory/src/components/Header/header.jsx
+++ employee-directory/src/components/Header/header.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-import "./style.css";
+import "./styles/header.css";
 
 const Header = () => {
   return (
-      <>
-
-    <h1 className="text-center">Employee Directory</h1>
-            <p className="lead">Click on carrots to filter by heading or use the search box to narrow your results.</p>
+      <div className="header">
+        <h1 className="text-center">Employee Directory</h1>
+        <p>Click on carrots to filter by heading or use the search box to narrow your results.</p>
+  </div>
   );
 };
-
-export default Header;
