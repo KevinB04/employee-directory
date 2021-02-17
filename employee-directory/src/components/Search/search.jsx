c7a7bb34dd47ecbbe4799c0cbc596ae71d15619b
Kevin Buttimer <kbuttimer4@gmail.com>
2021-02-17 16:57:33 -0500

    Creating search bar for search.jsx file

--- employee-directory/src/components/Search/search.jsx
+++ employee-directory/src/components/Search/search.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Search = () => {
-  return (
-    <div>
-      <h1>Search</h1>
-    </div>
-  );
-};
+class Search extends Component {
+  render() {
+    return (
+      <div className="row justify-content-center">
+        <form>
+          <div className="form-group">
+            <input
+              className="form control search-bar"
+              type="text"
+              placeholder="Search..."
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default Search;
